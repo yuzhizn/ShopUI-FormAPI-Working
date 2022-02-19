38eb0d0efc4b6238187e0c9cfcae45462b8039ca
AlexPads <37447608+AlexPads@users.noreply.github.com>
2022-02-19 17:46:53 -0500

    BedrockEconomy Update
    - Version Bump

--- src/AlexPads/CustomShopUI/economy/BedrockEconomyIntegration.php
+++ src/AlexPads/CustomShopUI/economy/BedrockEconomyIntegration.php
@@ -41,27 +41,11 @@ final class BedrockEconomyIntegration implements EconomyIntegration{
 		BedrockEconomyAPI::getInstance()->addToPlayerBalance($player->getName(), (int) ceil($money));
 	}
 
+	//Thanks for the Repair @cooldogedev
 	public function removeMoney(Player $player, float $money, Closure $callback) : void{
-		/**
-		 * SAFETY NOTICE
-		 * This method executes two asynchronous processes simultaneously:
-		 * 		getPlayerBalance(), subtractFromPlayerBalance()
-		 *
-		 * It is completely possible for a race condition to happen and an unexpected value
-		 * be subtracted as a consequence.
-		 * BedrockEconomy lets player balances go below 0, so directly invoking
-		 * subtractFromPlayerBalance() is not possible.
-		 */
-		$money_int = (int) ceil($money);
-		$this->getMoney($player, static function(float $balance) use($player, $money_int, $callback) : void{
-			if($balance >= $money_int){
-				BedrockEconomyAPI::getInstance()->subtractFromPlayerBalance($player->getName(), $money_int, ClosureContext::create(static function(bool $success) use($callback) : void{
-					$callback($success);
-				}));
-			}else{
-				$callback(false);
-			}
-		});
+		BedrockEconomyAPI::getInstance()->subtractFromPlayerBalance($player->getName(), (int) ceil($money), ClosureContext::create(static function(bool $success) use($callback) : void{
+			$callback($success);
+		}));
 	}
 
 	public function formatMoney(float $money) : string{
